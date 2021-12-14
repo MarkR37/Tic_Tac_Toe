883ea79e2ff5d8f3fa36feed39300c18be5d1099
Mark <mdroth7@gmail.com>
2021-12-14 02:23:23 -0500

    created a currentPlayer variable to delete duplicate logic.

--- src/runGame.java
+++ src/runGame.java
@@ -5,12 +5,10 @@ public class runGame {
 
     public static final char SPACE = '-';
     private final char[][] board = new char[3][3];
-    private final Scanner userInput = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
     private String winner;
-    private String takeUserInput;
-    private String wonOrTie;
-    private String Player1 = "Not Playing";
-    private String Player2 = "Not Playing";
+    private String userInput;
+    private String currentPlayer = "";
     private int numTurns = 0;
 
     private static final String INSTRUCTIONS = """
@@ -44,112 +42,62 @@ public class runGame {
     }
 
     private void updateBoard() {
-        if (Player1.equals("Playing") || Player2.equals("Playing")) {
-            int x = takeUserInput.charAt(0) - 48; //returns 0 of player 1
-            int y = takeUserInput.charAt(1) - 48; //returns 1
-            if (board[x][y] == 'X' || board[x][y] == '0') {
-                System.out.println("Already Chosen, please try again: ");
-                takeUserInput = userInput.next();
-                updateBoard();
-            }
-            if(Player1.equals("Playing")) {
-                if (board[x][y] == SPACE) {
-                    board[x][y] = '0';
-                }
-            }
-            if(Player2.equals("Playing")) {
-                if(board[x][y] == SPACE) {
-                    board[x][y] = 'X';
-                }
-            }
+        int x = userInput.charAt(0) - 48; //returns 0 of player 1
+        int y = userInput.charAt(1) - 48; //returns 1
+        if (board[x][y] == 'X' || board[x][y] == '0') {
+            System.out.println("Already Chosen, please try again: ");
+            userInput = scanner.next();
+            updateBoard();
+        } else if (currentPlayer.equals("Player 1")) {
+            board[x][y] = '0';
+        } else {
+            board[x][y] = 'X';
         }
     }
 
-    private void setPlayer() {
-        System.out.println("Please Choose player 1(0) or 2(X) by typing 1 or 2: ");
-        int z = userInput.nextInt();
-        if(z < 1 || z > 2) { //Checking to see if the user selected any other number other than 1 or 2
-            setPlayer();
-        }
-    }
-
-    private String userWon(char x) {
-        if(board[0][0] == x && board[0][1] == x && board[0][2] == x) {
-            return "won";
-        }
-        if(board[1][0] == x && board[1][1] == x && board[1][2] == x) {
-            return "won";
-        }
-        if(board[2][0] == x && board[2][1] == x && board[2][2] == x) {
-            return "won";
-        }
-        if(board[0][0] == x && board[1][0] == x && board[2][0] == x) {
-            return "won";
-        }
-        if(board[0][1] == x && board[1][1] == x && board[2][1] == x) {
-            return "won";
-        }
-        if(board[0][2] == x && board[1][2] == x && board[2][2] == x) {
-            return "won";
-        }
-        if(board[0][0] == x && board[1][1] == x && board[2][2] == x) {
-            return "won";
-        }
-        if(board[0][2] == x && board[1][1] == x && board[2][0] == x) {
-            return "won";
-        }
-        return "";
-    }
-
-    private void playingStatus(String doesP1, String doesP2) {
-        if(doesP1.equals("Playing")) {
-            System.out.println("Player 1: ");
-        }
-        if(doesP2.equals("Playing")) {
-            System.out.println("Player 2: ");
-        }
-        takeUserInput = userInput.next();
-        updateBoard();
-        Player1 = "Not Playing";
-        Player2 = "Not Playing";
-        printBoard();
-        numTurns++;
+    private boolean userWon(char x) {
+        return (board[0][0] == x && board[0][1] == x && board[0][2] == x) ||
+                (board[1][0] == x && board[1][1] == x && board[1][2] == x) ||
+                (board[2][0] == x && board[2][1] == x && board[2][2] == x) ||
+                (board[0][0] == x && board[1][0] == x && board[2][0] == x) ||
+                (board[0][1] == x && board[1][1] == x && board[2][1] == x) ||
+                (board[0][2] == x && board[1][2] == x && board[2][2] == x) ||
+                (board[0][0] == x && board[1][1] == x && board[2][2] == x) ||
+                (board[0][2] == x && board[1][1] == x && board[2][0] == x);
     }
 
     private void startGame() {
         System.out.println(INSTRUCTIONS);
-        setPlayer();
         initBoard();
         printBoard();
-        while(true) {
-            Player1 = "Playing";
-            playingStatus(Player1, Player2);
-            wonOrTie = userWon('0');
-            if(wonOrTie.equals("won")){
+        currentPlayer = "Player 1";
+        while (true) {
+            System.out.println(currentPlayer + ": ");
+            userInput = scanner.next();
+            updateBoard();
+            printBoard();
+            numTurns++;
+            if (currentPlayer.equals("Player 1") && userWon('0')) {
                 winner = "Player 1";
                 break;
-            }
-            if(numTurns == 9) {
-                wonOrTie = "tie";
-                break;
-            }
-            Player2 = "Playing";
-            playingStatus(Player1, Player2);
-            wonOrTie = userWon('X');
-            if(wonOrTie.equals("won")){
+            } else if (currentPlayer.equals("Player 2") && userWon('X')) {
                 winner = "Player 2";
                 break;
             }
-            if(numTurns == 9) {
-                wonOrTie = "tie";
+            if (numTurns == 9) {
+                winner = "tie";
                 break;
             }
-            printBoard(); 
+            if (currentPlayer.equals("Player 1")) {
+                currentPlayer = "Player 2";
+            } else {
+                currentPlayer = "Player 1";
+            }
         }
     }
 
     private void endGame() {
-        if(wonOrTie.equals("tie")) {
+        if(winner.equals("tie")) {
             System.out.println("You both Tied!");
         } else {System.out.println(winner + " won");}
     }
